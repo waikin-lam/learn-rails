a5e017d3e8692601431be86eb6aa1f4baa8a84df
Lam Wai Kin <ryanlwk@gmail.com>
2016-01-03 22:59:43 +0800

    assets compiled for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+    # email enabled in production
+    config.action_mailer.default_url_options = { :host => ENV["test001-learn-rails.herokuapp.com"] }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.default :charset => "utf-8"
+    config.action_mailer.smtp_settings = {
+        address: "smtp.gmail.com",
+        port: 587,
+        domain: ENV["test001-learn-rails.herokuapp.com"],
+        authentication: "plain",
+        enables_starttls_auto: true,
+        user_name: ENV["GMAIL_USERNAME"],
+        password: ENV["GMAIL_PASSWORD"]
+        }
 end
